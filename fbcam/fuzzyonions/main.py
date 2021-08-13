2bab9baa73de7c3a7da425eae1b16ba6a55f744c
Damien Goutte-Gattat <dpg44@cam.ac.uk>
2021-08-13 19:32:29 +0100

    Initial commit.
    This is FuzzyOnions (fzo), a set of helper scripts and Python modules
    for the scRNAseq project in FlyBase.

---
+++ fbcam/fuzzyonions/main.py
@@ -0,0 +1,105 @@
+# fuzzy-onions - FlyBase scRNAseq scripts
+# Copyright © 2021 Damien Goutte-Gattat
+#
+# Redistribution and use of this script, with or without modifications,
+# is permitted provided that the following conditions are met:
+#
+# 1. Redistributions of this script must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import logging
+from configparser import ConfigParser
+
+import click
+from click_shell import shell
+
+from fbcam.fuzzyonions import __version__
+from fbcam.fuzzyonions.scea import FileStore
+
+prog_name = "fuzzyonions"
+prog_notice = f"""\
+{prog_name} {__version__}
+Copyright © 2021 Damien Goutte-Gattat
+
+This program is released under the terms of the 1-clause BSD licence.
+"""
+
+
+class FzoContext(object):
+
+    def __init__(self, config_file, section):
+        self._config = ConfigParser()
+        self._config.read(config_file)
+
+        self._section = section
+        self._store = None
+
+    @property
+    def raw_store(self):
+        if not self._store:
+            d = self._config.get(self._section, 'raw_store')
+            self._store = FileStore(d)
+        return self._store
+
+
+@shell(context_settings={'help_option_names': ['-h', '--help']},
+       prompt="fzo >")
+@click.version_option(version=__version__, message=prog_notice)
+@click.option('--config', '-c', type=click.Path(exists=True),
+              default='{}/config'.format(click.get_app_dir('fuzzyonions')),
+              help="Path to an alternative configuration file.")
+@click.option('--section', '-s', default='default',
+              help="Name of the configuration file section to use.")
+@click.pass_context
+def main(ctx, config, section):
+    """Helper scripts for the FlyBase scRNAseq project."""
+
+    logging.basicConfig(format="fzo: %(module)s: %(message)s",
+                        level=logging.INFO)
+
+    context = FzoContext(config, section)
+    ctx.obj = context
+
+
+@main.command()
+@click.argument('dsid')
+@click.pass_obj
+def download(ctx, dsid):
+    """Download a SCEA dataset.
+    
+    This command fetches the data for the specified dataset from the
+    SCEA server.
+    """
+
+    ctx.raw_store.get(dsid)
+
+
+@main.command()
+@click.pass_obj
+def list(ctx):
+    """List the datasets in the local store."""
+
+    n = len(ctx.raw_store.datasets)
+    if n == 0:
+        print("No local datasets available.")
+        return
+
+    print(f"{n} dataset(s) available:")
+    for d in ctx.raw_store.datasets:
+        print(f"{d.id}")
+
+
+if __name__ == '__main__':
+    main()
---
+++ fbcam/fuzzyonions/scea.py
@@ -0,0 +1,185 @@
+# fuzzy-onions - FlyBase scRNAseq scripts
+# Copyright © 2021 Damien Goutte-Gattat
+#
+# Redistribution and use of this script, with or without modifications,
+# is permitted provided that the following conditions are met:
+#
+# 1. Redistributions of this script must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+"""Helper module to deal with SCEA datasets."""
+
+from enum import IntFlag
+import logging
+import os
+from zipfile import ZipFile
+
+import requests
+
+
+class DataType(IntFlag):
+    EXPERIMENT_METADATA = 1,
+    EXPERIMENT_DESIGN = 2,
+    CLUSTERS = 4,
+    MARKER_GENES = 8,
+    NORMALISED_EXPRESSION_DATA = 16,
+    RAW_EXPRESSION_DATA = 32
+
+
+class FileStore(object):
+    """Represents a local file-based store for SCEA data."""
+
+    def __init__(self, directory):
+        self._directory = directory
+        self._downloader = Downloader(directory)
+        self._datatypes = (DataType.EXPERIMENT_DESIGN
+                           | DataType.EXPERIMENT_METADATA
+                           | DataType.NORMALISED_EXPRESSION_DATA
+                           | DataType.RAW_EXPRESSION_DATA)
+
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+
+        self._datasets = None
+
+    @property
+    def datasets(self):
+        """Gets all the datasets in the store."""
+
+        if not self._datasets:
+            self._refresh()
+        return self._datasets.values()
+
+    def get(self, dsid, download=True):
+        """Gets a single dataset.
+        
+        :param dsid: the dataset ID
+        :param download: if True (the default), the dataset will be
+            downloaded if it is not already available locally
+        """
+
+        if not self._datasets:
+            self._refresh()
+
+        if dsid not in self._datasets.keys():
+            if not download:
+                return None
+            elif not self._downloader.get(dsid, self._datatypes):
+                return None
+            self._datasets[dsid] = Dataset(os.path.join(self._directory, dsid))
+
+        return self._datasets[dsid]
+
+    def _refresh(self):
+        self._datasets = {}
+        for dsid in os.listdir(self._directory):
+            self._datasets[dsid] = Dataset(os.path.join(self._directory, dsid))
+
+
+class Dataset(object):
+    """Represents a single SCEA dataset."""
+
+    FILES = {
+        DataType.EXPERIMENT_METADATA: '{}.idf.txt',
+        DataType.EXPERIMENT_DESIGN: 'experiment-design.tsv',
+        DataType.CLUSTERS: '{}.clusters.tsv',
+        DataType.MARKER_GENES: '{}-marker-genes-files',
+        DataType.NORMALISED_EXPRESSION_DATA: '{}.aggregated_filtered_normalised_counts.mtx',
+        DataType.RAW_EXPRESSION_DATA: '{}.aggregated_filtered_counts.mtx'
+        }
+
+    def __init__(self, directory):
+        self._directory = directory
+        self._id = os.path.basename(directory)
+        self._files = os.listdir(directory)
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def available_data(self):
+        """Gets a list of locally-available data for this dataset."""
+
+        return [d for d, f in Dataset.FILES.items() if f in self._files]
+
+
+class Downloader(object):
+    """Helper object to download data from the SCEA."""
+
+    BASE_URL = 'https://www.ebi.ac.uk/gxa/sc/experiment'
+    FILES = {
+        DataType.EXPERIMENT_METADATA: ('experiment-metadata', True),
+        DataType.EXPERIMENT_DESIGN: ('experiment-design', False),
+        DataType.CLUSTERS: ('cluster', False),
+        DataType.MARKER_GENES: ('marker-genes', True),
+        DataType.NORMALISED_EXPRESSION_DATA: ('normalised', True),
+        DataType.RAW_EXPRESSION_DATA: ('quantification-raw', True)
+        }
+
+    def __init__(self, destination, base=BASE_URL):
+        """Creates a new Downloader object.
+        
+        :param destination: the directory to store the downloaded
+            files into
+        :param base: an alternative URL to download from
+        """
+
+        self._outdir = destination
+        self._baseurl = base
+
+    def get(self, dsid, data_type):
+        """Download a dataset file from the SCEA.
+        
+        :param dsid: the dataset ID
+        :param data_type: the type of data to download
+        :return: True if the download was successful, False otherwise
+        """
+
+        if not self._check_dataset(dsid):
+            return False
+
+        for value in DataType:
+            if data_type & value:
+                filename, is_zip = Downloader.FILES[value]
+                self._download(dsid, filename, is_zip)
+
+    def _check_dataset(self, dsid):
+        url = f'{self._baseurl}s/{dsid}/results'
+        with requests.head(url) as response:
+            return response.ok
+
+    def _download(self, dsid, file_type, is_zip=False):
+        z = 'zip' if is_zip else ''
+        ext = 'zip' if is_zip else 'tsv'
+        url = f'{self._baseurl}/{dsid}/download/{z}?fileType={file_type}'
+        destdir = f'{self._outdir}/{dsid}'
+        dest = f'{destdir}/{file_type}.{ext}'
+
+        if not os.path.exists(destdir):
+            os.mkdir(destdir)
+
+        logging.info(f"Downloading {url}")
+
+        r = requests.get(url, stream=True)
+        with open(dest, 'wb') as fd:
+            for chunk in r.iter_content(chunk_size=None):
+                fd.write(chunk)
+
+        if is_zip:
+            archive = ZipFile(dest)
+            for member in archive.namelist():
+                archive.extract(member, path=destdir)
+            os.unlink(dest, dir_fd=None)
