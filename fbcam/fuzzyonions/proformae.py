214f1904fc099bec76691599f5d6d05d5416b788
Damien Goutte-Gattat <dpg44@cam.ac.uk>
2022-04-05 20:20:22 +0100

    Remove the proforma template system.
    We no longer use the templating system to fill proforma, so we can get
    rid of that code.

--- fbcam/fuzzyonions/proformae.py
+++ fbcam/fuzzyonions/proformae.py
@@ -28,122 +28,6 @@ class ProformaType(Enum):
     DATASET = 1
 
 
-TEMPLATES = {
-    'pub_mini': {
-        'proforma': ProformaType.PUB_MINI,
-        'contents': [
-            ('P22', 'TODO: FBrfXXXXXXX'),
-            ('P2', 'TODO: <Journal abbreviation>'),
-            ('P19', None)
-            ]
-        },
-    'dataset/project': {
-        'proforma': ProformaType.DATASET,
-        'contents': [
-            ('LC1f', 'new'),
-            ('LC1a', None),
-            ('LC6g', 'TODO: Single-cell RNA-seq study of <tissue and stage> [upon condition]'),
-            ('LC6a', None),
-            ('LC2a', 'project ; FBcv:0003023'),
-            ('LC2b', 'transcriptome ; FBcv:0003034'),
-            ('LC13a', None),
-            ('LC13b', None),
-            ('LC13c', 'TODO: [GO terms for biological processes studied]'),
-            ('LC13d', None),
-            ('LC4a', 'Dmel'),
-            ('LC4i', None),
-            ('LC6d', 'N'),
-            ('LC11m', 'TODO: <FBcv terms from \'study design\' (FBcv:0003130)>'),
-            ('LC6b', 'TODO: <How the biological material was processed>'),
-            ('LC11c', 'TODO: <How the mRNA libraries were sequenced>'),
-            ('LC11e', 'TODO: <How the sequencing data were analysed>'),
-            ('LC7a', 'The EMBL-EBI\'s Single Cell Expression Atlas provides cell-level annotations, clustering data, raw and normalised read counts, and putative marker genes.'),
-            ('LC99a', None),
-            ('LC99b', 'EMBL-EBI Single Cell Expression Atlas Datasets'),
-            ('LC8c', 'TODO: [Name of the research group](URL of their website)'),
-            ('LC10', None)
-            ]
-        },
-    'dataset/biosample': {
-        'proforma': ProformaType.DATASET,
-        'contents': [
-            ('LC1f', 'new'),
-            ('LC1a', None),
-            ('LC6g', None),
-            ('LC2a', 'biosample ; FBcv:0003024'),
-            ('LC2b', None),
-            ('LC3', None),
-            ('LC4a', 'Dmel'),
-            ('LC4h', 'TODO: [as needed]'),
-            ('LC4f', 'TODO: [as needed]'),
-            ('LC4g', None),
-            ('LC4i', None),
-            ('LC12a', 'TODO: [as needed]'),
-            ('LC12b', 'TODO: [as needed]'),
-            ('LC6d', 'N'),
-            ('LC11m', None),
-            ('LC11a', None),
-            ('LC10', None)
-            ]
-        },
-    'dataset/assay': {
-        'proforma': ProformaType.DATASET,
-        'contents': [
-            ('LC1f', 'new'),
-            ('LC1a', None),
-            ('LC6g', None),
-            ('LC2a', 'assay ; FBcv:0003025'),
-            ('LC2b', None),
-            ('LC3', None),
-            ('LC14a', None),
-            ('LC14d', 'TODO: [LC1a symbol of technical reference assay, if relevant]'),
-            ('LC14e', 'TODO: [LC1a symbol of biological reference assay, if relevant]'),
-            ('LC4a', 'Dmel'),
-            ('LC6d', 'N'),
-            ('LC6e', None),
-            ('LC6f', None),
-            ('LC11m', 'TODO: <FBcv terms from \'assay method\' (FBcv:0003208)>'),
-            ('LC10', None)
-            ]
-        },
-    'dataset/result': {
-        'proforma': ProformaType.DATASET,
-        'contents': [
-            ('LC1f', 'new'),
-            ('LC1a', None),
-            ('LC6g', None),
-            ('LC2a', 'result ; FBcv:0003026'),
-            ('LC2b', None),
-            ('LC3', None),
-            ('LC14b', None),
-            ('LC14h', 'Dmel R6.32'),
-            ('LC4a', 'Dmel'),
-            ('LC6d', 'N'),
-            ('LC6e', None),
-            ('LC6f', None),
-            ('LC10', None)
-            ]
-        },
-    'dataset/subresult': {
-        'proforma': ProformaType.DATASET,
-        'contents': [
-            ('LC1f', 'new'),
-            ('LC1a', None),
-            ('LC6g', None),
-            ('LC2a', 'result ; FBcv:0003026'),
-            ('LC2b', None),
-            ('LC3', None),
-            ('LC4a', 'Dmel'),
-            ('LC4g', None),
-            ('LC6d', 'N'),
-            ('LC6e', None),
-            ('LC6f', None),
-            ('LC10', None)
-            ]
-        }
-    }
-
-
 class ProformaGeneratorBuilder(object):
 
     _PROFORMAE = {
@@ -156,10 +40,7 @@ class ProformaGeneratorBuilder(object):
         self._output = output
         self._generators = {}
 
-    def get_generator(self, proforma_type=None, template=None):
-        if template is not None:
-            proforma_type = TEMPLATES[template]['proforma']
-            template = TEMPLATES[template]['contents']
+    def get_generator(self, proforma_type=ProformaType.DATASET):
 
         if not proforma_type in self._generators:
             filename, name = self._PROFORMAE[proforma_type]
@@ -169,29 +50,23 @@ class ProformaGeneratorBuilder(object):
             self._generators[proforma_type] = generator
 
         generator = self._generators[proforma_type]
-        if template is not None:
-            generator.set_template(template)
 
         return generator
 
 
 class ProformaGenerator(object):
 
-    def __init__(self, source, name, output, template=None):
+    def __init__(self, source, name, output):
         self._source = source
         self._name = name
         self._fields = None
         self._fp = output
-        self._template = template
 
     @property
     def fields(self):
         if not self._fields:
             self._fields = self._parse_proforma(self._source, self._name)
         return self._fields
-
-    def set_template(self, template):
-        self._template = template
 
     def _parse_proforma(self, filename, name):
         res = {}
@@ -240,17 +115,3 @@ class ProformaGenerator(object):
             return len(value) == 0
         else:
             return False
-
-    def fill_template(self, values={}, template=None):
-        if template is None:
-            template = self._template
-
-        self.write_header()
-
-        for field, value in template:
-            if field in values:
-                if values[field] != '<DEFAULT>':
-                    value = values[field]
-            self.write_field(field, value)
-
-        self.write_separator()
